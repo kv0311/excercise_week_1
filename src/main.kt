34b40db2189da45a78b0f3c99954fa2a1acc1052
kv.0311 <kvxi98@gmail.com>
2020-04-18 20:47:10 +0700

    commit

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val string_1 = "abcdefghi"
+    val string_2 = "abcdeghfi"
+    var count =0
+        for ((key_1,value_1 ) in string_1.withIndex()){
+            for ((key_2,value_2 ) in string_2.withIndex()){
+                if(key_1==key_2){
+                    if(value_1==value_2){
+                        count=count+1
+                    }
+                }
+            }
+        }
+    println(count)
 }
